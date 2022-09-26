cbefd269e7704087754ceee34a23a30328faaf77
BadriNarayananKR <saathvika_badri@yahoo.co.in>
2022-09-26 13:45:45 +0530

    my first commit

--- Training1/default.aspx.vb
+++ Training1/default.aspx.vb
@@ -3,6 +3,7 @@
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
         Response.Write("Hello! Welcome to DevOps Training on " & Today.Date)
+        Response.Write("Starting my first commit")
     End Sub
 
 End Class
